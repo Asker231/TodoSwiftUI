96c269d5519371916f28bfe63423ce3c51103b39
Asker <kotsevasker0@gmail.com>
2023-04-30 12:18:25 +0300

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by ASKER on 30.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
